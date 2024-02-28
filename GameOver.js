fe7aec2533201249584998f17a8f7d6e373aa8ab
Andrew Wladis <andrewwladis@gmail.com>
2024-02-28 08:43:50 -0500

    Added leaderboard adding

--- GameOver.js
+++ GameOver.js
@@ -1,12 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, Alert } from 'react-native';
 import styles from './Styles';
 import { useEffect, useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import randomColor from './functions/randomColor';
 
 export default function GameOver({ setPage, diffuculty, finalScore, highScores }) {
+    const [yourName, setYourName] = useState(null);
     const diffucultyArray = ['Easy', 'Medium', 'Hard'];
+
+    const showAlertWithInput = () => {
+        Alert.prompt(
+            'Enter your name for the leaderboard.',
+            'Enter your name',
+            (text) => setYourName(text),
+            'plain-text',
+        );
+    };
+
+    useEffect(() => {
+        if (highScores !== null && highScores !== undefined && highScores.length !== 0) {
+            if (highScores[highScores.length - 1].score < finalScore) {
+                showAlertWithInput()
+            }
+        }
+    }, [highScores]);
+
+    useEffect(() => {
+        if (yourName !== null && yourName !== "") {
+            if (yourName.length > 10) {
+                setYourName(yourName.slice(0, 10))
+            }
+
+            const addToLeaderboard = async () => {
+                try {
+                    const response = await fetch('https://correct-boxd-backend.onrender.com/cc-add-to-leaderboard', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            name: yourName,
+                            score: finalScore
+                        }),
+                    });
+
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
+                    }
+                } catch (error) {
+                    console.error('An error occurred:', error.message);
+                    // Handle error accordingly
+                }
+            };
+
+            addToLeaderboard();
+        }
+    }, [yourName])
 
     return (
         <LinearGradient style={styles.container} colors={[randomColor(), randomColor()]}>
